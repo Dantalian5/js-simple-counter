45df4d5fb436393fbe4418e8d695dc6e992fd9bd
Marcos Ernesto Planos Valenzuela <87524702+Dantalian5@users.noreply.github.com>
2023-10-22 20:22:57 +0200

    dev

--- src/assets/js/script.js
+++ src/assets/js/script.js
@@ -3,3 +3,5 @@ const hamburguerMenu = document.getElementById("js-hamburguerMenu");
 hamburguerMenu.addEventListener("click", (event) => {
 	hamburguerMenu.classList.toggle("active");
 });
+
+// ! make menu disapear wen user click outside
