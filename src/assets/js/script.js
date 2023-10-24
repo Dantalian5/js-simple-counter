e72fdc4d09275f037c8152dae8b9f88345836481
Marcos Ernesto Planos Valenzuela <87524702+Dantalian5@users.noreply.github.com>
2023-10-24 19:32:37 +0200

    dev script

--- src/assets/js/script.js
+++ src/assets/js/script.js
@@ -1,4 +1,5 @@
 const hamburguerMenu = document.getElementById("js-hamburguerMenu");
+const inputCounter = document.getElementById("js-inputCounter");
 
 hamburguerMenu.addEventListener("click", (event) => {
 	hamburguerMenu.classList.toggle("active");
@@ -9,5 +10,15 @@ document.addEventListener("click", (event) => {
 		event.target.closest("#js-menuItems")
 	) {
 		hamburguerMenu.classList.remove("active");
+	}
+});
+inputCounter.addEventListener("change", (event) => {
+	let value = inputCounter.value;
+	if (value < 0 || value == "") {
+		inputCounter.value = "00";
+	} else if (value > 60) {
+		inputCounter.value = 60;
+	} else if (value < 10) {
+		inputCounter.value = "0" + (+inputCounter.value).toString();
 	}
 });
